8848b160a95322839fb9a94841e656b3d333e893
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-04-10 20:38:52 +0200

    add map container (leaflet) in main

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,10 +1,19 @@
-const Map = { { IpInfo }: MapProps} {
+import { MapContainer, TileLayer, Marker, Popup } from 'react-leaflet';
+import { Icon } from 'leaflet';
+//const Map = { { IpInfo }: MapProps} {
     const defaultPosition : [number, number] = [40, 0];
-    const position: [number, number] = ipInfo ? [IpInfo.ipApi.lat, IpInfo.ipApi.lon] : defaultPosition;
+//    const position: [number, number] = ipInfo ? [IpInfo.ipApi.lat, IpInfo.ipApi.lon] : defaultPosition;
 
     return (
-        <MapContainer>
-
+        <MapContainer
+            center={position}
+            zoom={13}
+            style={{ height: '500px', width: '100%'}}    
+        >
+             <TileLayer
+                url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+                attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            />
         </MapContainer>
     )
 }
