e5062f1d089dc008f096c3bfb7fae791df6e4e90
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-04-10 20:43:57 +0200

    add comment

--- src/types/IpApiRepsonse.ts
+++ src/types/IpApiRepsonse.ts
@@ -1,3 +1,21 @@
+/*
+{
+    "status": "success",
+    "country": "Spain",
+    "countryCode": "ES",
+    "region": "CT",
+    "regionName": "Catalonia",
+    "city": "Barcelona",
+    "zip": "08028",
+    "lat": 41.383,
+    "lon": 2.1181,
+    "timezone": "Europe/Madrid",
+    "isp": "Digi Spain Telecom S.L.U.",
+    "org": "",
+    "as": "AS57269 DIGI SPAIN TELECOM S.L.",
+    "query": "188.26.220.221"
+}
+*/
 export interface IpApiResponse {
     status: string;
     country: string;
