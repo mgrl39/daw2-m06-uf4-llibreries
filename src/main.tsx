5d07a9320d9e0127e7c80cfca0daced34ccf6c29
usuario <170405410+mgrl39@users.noreply.github.com>
2025-05-01 10:20:46 +0200

    improve comments

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,9 @@
+/*
+ * Importa StrictMode de React per habilitar el mode estricte
+ * Importa createRoot de react-dom/client per renderitzar l'aplicació
+ * Importa els estils de Bootstrap i personalitzats
+ * Importa el component principal de l'aplicació
+ */
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
@@ -5,6 +11,10 @@ import "bootstrap-icons/font/bootstrap-icons.css";
 import "./assets/styles.css";
 import App from "./App";
 
+/**
+ * Crea l'arrel de l'aplicacio.
+ * Envolta l'aplicació en StrictMode per detectar problemes potencials
+ */
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
